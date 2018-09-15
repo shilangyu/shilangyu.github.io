1ed6db9d8e2c8682abb2037a083f1e215be64738
Marcin Wojnarowski <marcin.wojnarowski@ibm.com>
2018-09-15 10:52:53 +0200

    added theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import hashHistory from '../history'
 import { Router, Route, Switch } from 'react-router-dom'
 
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import theme from './../styles/theme'
+
+import Index from './pages/Index.jsx'
+
 
 class App extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        <Switch>
-          <Route exact path="/" component={() => <div>lul</div>} />
-          <Route exact path="/s" component={() => <div>monika lal</div>} />
-        </Switch>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router history={hashHistory}>
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/s" component={() => <div>monika lal</div>} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
     )
   }
 }
 
-export default App;
+export default App
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff80ab',
+    },
+    secondary: {
+      main: '#00b8d4',
+    },
+  },
+})
+
+export default theme
