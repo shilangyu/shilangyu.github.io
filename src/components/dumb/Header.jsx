f18410953d41b8d8d85d13c0a1e9a9ea8ab4deee
Marcin Wojnarowski <marcin.wojnarowski@ibm.com>
2018-09-17 21:49:05 +0200

    more explicit import for reduced bundle size

--- src/components/dumb/Header.jsx
+++ src/components/dumb/Header.jsx
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import { ArrowDropDown } from '@material-ui/icons'
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 
