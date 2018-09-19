bf1fc6d9e1e914ba740fb8b652337712b3efb0c8
Marcin Wojnarowski <marcin.wojnarowski@ibm.com>
2018-09-19 17:18:52 +0200

    added machine learning section

--- src/components/portfolio/2018/posts.jsx
+++ src/components/portfolio/2018/posts.jsx
@@ -1,3 +1,5 @@
+import React, { Fragment } from 'react'
+
 export default [
 	{
 		title: 'snek.io',
@@ -21,6 +23,33 @@ export default [
 			{
 				title: 'Multiplayer',
 				content: 'Each player can either host or join a game, by entering a hosting number and your nickname. After that a lobby window pops up where you can see all players\' nicknames in this room.'
+			}
+		]
+	},
+	{
+		title: 'machine learning',
+		posts: [
+			{
+				title: 'Description',
+				content: <Fragment>It was a personal development project: learning more about how Artificial intelligence works. My goal was to fully understand and write code from the ground up without using existing libraries. Below you can find some projects I worked on using Machine Learning algorithms. <br />
+					Disclaimer: graphs are only to explain the very basic idea.
+				</Fragment>
+			},
+			{
+				title: 'Genetic Algorithm',
+				content: 'This algorithm focuses on perfecting something, just like in nature, we evolve and the fittest survives. Its comparable to how humans inherit features through genetics. In this example many 10PRINT designs are randomly created then after you chose your favorite 2, new ones are generated based on the previous bests.'
+			},
+			{
+				title: 'Neural Network',
+				content: 'This algorithm is the `brain`. Based on inputs it gives an output after processing the information. In this example I build in C# a program that first takes an image of a track to be created. Then from a given point cars are started and their goal is to go as far as possible. As shown on the picture, a car recieves distance inputs. It scans how far a wall is at all 8 directions and decides where to move next.'
+			},
+			{
+				title: 'Neuroevolution',
+				content: 'This algorithm merges the two previous ones. Now the `brain` gets better over time, the arrows here will represent the IQ level of a network. In this example I created a simulation of some balls trying to learn how to jump over the obstacles. Each of the 100 balls get the input of how far and how high is the obstacle, and how fast is it moving. Each of them have a unique set of arrows that help them decide whether they should jump or not. After all 100 balls are dead (hit a wall), a new, better generation of 100 new balls is made based & mutated off the best balls in the previous generation.'
+			},
+			{
+				title: 'TicTacToe',
+				content: 'A simple AI I made that learns by playing, if a move he makes is bad he remembers it and never does it again.'
 			}
 		]
 	}
