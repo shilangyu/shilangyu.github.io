f172c4f942b693f716f8dae87fe2d1ee587e0aca
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-09-29 14:17:42 +0200

    better padding for smaller screens #3

--- src/components/dumb/Container.jsx
+++ src/components/dumb/Container.jsx
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid'
 
 export default props =>
 	<Grid container justify="center">
-		<Grid item xs={12} sm={9} md={8} lg={8}>
+		<Grid item xs={11} sm={9} md={8} lg={8}>
 			{props.children}
 		</Grid>
 	</Grid>
