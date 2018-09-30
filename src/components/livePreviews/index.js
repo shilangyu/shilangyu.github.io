ef69831d656190b250756b81eb57c272e98f7f09
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-09-30 18:58:30 +0200

    created live-previews #4

--- src/components/livePreviews/index.js
+++ src/components/livePreviews/index.js
@@ -1,14 +1,73 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+import urls from '../../constants/urls'
+
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 3
+  },
+  card: {
+    width: 500,
+  },
+  media: {
+    objectFit: 'cover',
+  },
+})
 
 class LivePreviews extends Component {
+  state = {
+    previews: [
+      { name: '10PRINT', desc: '10PRINT patterns generated based on your preferences', picSrc: '/assets/pics/live-previews/10PRINT.png', href: urls.external.livePreviews['10print']},
+      { name: 'tictactoe', desc: 'Playing against an AI that gets better the more it plays', picSrc: '/assets/pics/live-previews/tictactoe.png', href: urls.external.livePreviews.tictactoe},
+      { name: 'jumping balls NE', desc: 'Jumping balls that learn over time how to jump over the obstacles', picSrc: '/assets/pics/live-previews/jumping-balls.png', href: urls.external.livePreviews.jumpingBalls}
+    ]
+  }
   render() {
+    const { classes } = this.props
+    const { previews } = this.state
+
     return (
-      <Fragment>
-        cuntish
-      </Fragment>
+      <Grid container spacing={24} className={classes.root}>
+        {previews.map(preview => 
+          <Grid key={preview.href} xs={6} item>
+            <Card className={classes.card}>
+              <CardActionArea onClick={() => window.open(preview.href)}>
+                <CardMedia
+                  component="img"
+                  className={classes.media}
+                  image={preview.picSrc}
+                  title={preview.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="headline" component="h2">
+                    {preview.name}
+                  </Typography>
+                  <Typography component="p">
+                    {preview.desc}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary" onClick={() => window.open(urls.external.github + preview.href)}>
+                  Repository
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        )}
+      </Grid>
     )
   }
 }
 
-export default LivePreviews
+export default withStyles(styles)(LivePreviews)
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -3,10 +3,15 @@ export default {
 	livePreviews: '/live-previews',
 	portfolio: '/portfolio/:year',
 	external: {
-		github: 'https://github.com/clitcancer',
+		github: 'https://github.com/shilangyu',
 		materialui: 'https://material-ui.com',
 		p5: 'http://p5js.org',
 		drawio: 'https://www.draw.io/',
-		react: 'http://reactjs.org'
+		react: 'http://reactjs.org',
+		livePreviews: {
+			'10print': '/10PRINT',
+			jumpingBalls: '/jumping-balls',
+			tictactoe: '/tictactoe',
+		}
 	}
 }
