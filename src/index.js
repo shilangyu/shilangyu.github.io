dfc6508d1bfc876ff1286ebe35b48fb4e7cd4311
shilangyu <xmarcinmarcin@gmail.com>
2018-10-29 15:07:10 +0100

    custom scrollbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
 import registerServiceWorker from './registerServiceWorker'
+import './styles/scrollbar.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
