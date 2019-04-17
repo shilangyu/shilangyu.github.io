0cdc0d204d2fa37e992f391ae2430af08305e989
shilangyu <xmarcinmarcin@gmail.com>
2019-04-17 18:23:51 +0200

    Updates

---
+++ precache-manifest.af9a88141743cad130dc5ef2ba4efb50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00d668ce5bdb763f66e2",
+    "url": "/static/css/main.4170da9e.chunk.css"
+  },
+  {
+    "revision": "00d668ce5bdb763f66e2",
+    "url": "/static/js/main.32f0e662.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d5e231189fe988e454f",
+    "url": "/static/js/2.0d58b88c.chunk.js"
+  },
+  {
+    "revision": "31d54f6b1979598e80e32a21adc945db",
+    "url": "/index.html"
+  }
+];
