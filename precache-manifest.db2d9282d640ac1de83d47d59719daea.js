cc2b6fbbca9dc43b27547916cb913affbb864e5a
shilangyu <xmarcinmarcin@gmail.com>
2019-08-14 15:27:53 +0200

    Updates

---
+++ precache-manifest.db2d9282d640ac1de83d47d59719daea.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd81d6a9d9d11336bf0757429b2c65c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029c5d1c0ec86b4a29c0",
+    "url": "/static/js/2.b39ec678.chunk.js"
+  },
+  {
+    "revision": "148e45dbc73b3dbddc78",
+    "url": "/static/js/main.5b067ac4.chunk.js"
+  },
+  {
+    "revision": "9780aaafdcec93ae29fa",
+    "url": "/static/js/runtime~main.b2aa5f2a.js"
+  },
+  {
+    "revision": "e29fe00dfd3044bd29b10ad91acb3bbb",
+    "url": "/static/media/DR-dataset.e29fe00d.png"
+  },
+  {
+    "revision": "ffdd48af507af250efffc9124476bd6e",
+    "url": "/static/media/DR-mnist.ffdd48af.png"
+  },
+  {
+    "revision": "30886fae3e8ba6afda5251fb5a99ea49",
+    "url": "/static/media/DR-visualisation.30886fae.png"
+  },
+  {
+    "revision": "5b4b38aec2ccf7668d2be133d9a5d701",
+    "url": "/static/media/GA-graph.5b4b38ae.png"
+  },
+  {
+    "revision": "63c0ca8e71f6ae942db3d1202286e130",
+    "url": "/static/media/NE-graph.63c0ca8e.png"
+  },
+  {
+    "revision": "9a15265f2f2d11b4a9bf83eec931c778",
+    "url": "/static/media/NNcar-laser.9a15265f.png"
+  },
+  {
+    "revision": "636f8a0d466cab04b927601274943054",
+    "url": "/static/media/drawio.636f8a0d.png"
+  },
+  {
+    "revision": "52a8d2453da7990033b1197e9037662e",
+    "url": "/static/media/geneticts.52a8d245.svg"
+  },
+  {
+    "revision": "d9321b7c851fc0f4956af370743413e4",
+    "url": "/static/media/lessons-GA.d9321b7c.png"
+  },
+  {
+    "revision": "47155a76557a3a831177efea71d08f9f",
+    "url": "/static/media/lessons-perceptron.47155a76.jpg"
+  },
+  {
+    "revision": "cb2f65a2181811ba82d3db0ed0880d1c",
+    "url": "/static/media/materialui.cb2f65a2.svg"
+  },
+  {
+    "revision": "e72f4c33073c8fdfae39506d7efb07d9",
+    "url": "/static/media/p5js.e72f4c33.png"
+  },
+  {
+    "revision": "7e5057e4e10ded11ae85326487053ed9",
+    "url": "/static/media/page-graph.7e5057e4.png"
+  },
+  {
+    "revision": "7c0fdc490914ad5e54763db4ef955066",
+    "url": "/static/media/react.7c0fdc49.png"
+  },
+  {
+    "revision": "dea9183be33adbaf6afa79eef3266d29",
+    "url": "/static/media/snek-leaderboards.dea9183b.png"
+  },
+  {
+    "revision": "e9f0f439630cc9f8fad6256839540097",
+    "url": "/static/media/snek-menu.e9f0f439.png"
+  },
+  {
+    "revision": "b78b1d41d60b0d9a996ec4b3c2ad2815",
+    "url": "/static/media/snek-multiplayer.b78b1d41.png"
+  },
+  {
+    "revision": "ea62575d321e5207e21832d4bf67ac95",
+    "url": "/static/media/tictactoe.ea62575d.png"
+  }
+]);
