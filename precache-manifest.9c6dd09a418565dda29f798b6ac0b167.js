08de58e789eb3002c70c75e991d0b5707284363b
shilangyu <xmarcinmarcin@gmail.com>
2019-08-17 23:24:02 +0200

    Updates

---
+++ precache-manifest.9c6dd09a418565dda29f798b6ac0b167.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a466a449a80c7e6d3a0d2b9f2d6bdf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a4161dea50535e3d19",
+    "url": "/static/js/2.0f62bdeb.chunk.js"
+  },
+  {
+    "revision": "dc1a5f0afdcd5a2b2a46",
+    "url": "/static/js/main.e62cbc85.chunk.js"
+  },
+  {
+    "revision": "9780aaafdcec93ae29fa",
+    "url": "/static/js/runtime~main.b2aa5f2a.js"
+  },
+  {
+    "revision": "923304ad3ae6cc73383b047d72c9ac72",
+    "url": "/static/media/DR-dataset.923304ad.png"
+  },
+  {
+    "revision": "ffdd48af507af250efffc9124476bd6e",
+    "url": "/static/media/DR-mnist.ffdd48af.png"
+  },
+  {
+    "revision": "c46482c10f0d0f177541c30072f5b955",
+    "url": "/static/media/DR-visualisation.c46482c1.png"
+  },
+  {
+    "revision": "f273e8b84021a288e38c62fb8fb10288",
+    "url": "/static/media/GA-graph.f273e8b8.png"
+  },
+  {
+    "revision": "861f778cd8c6162d5d665a8e6487ace8",
+    "url": "/static/media/NE-graph.861f778c.png"
+  },
+  {
+    "revision": "7ab02160d427bfafe395a7ce7483f59b",
+    "url": "/static/media/NNcar-laser.7ab02160.png"
+  },
+  {
+    "revision": "636f8a0d466cab04b927601274943054",
+    "url": "/static/media/drawio.636f8a0d.png"
+  },
+  {
+    "revision": "52a8d2453da7990033b1197e9037662e",
+    "url": "/static/media/geneticts.52a8d245.svg"
+  },
+  {
+    "revision": "d9321b7c851fc0f4956af370743413e4",
+    "url": "/static/media/lessons-GA.d9321b7c.png"
+  },
+  {
+    "revision": "47155a76557a3a831177efea71d08f9f",
+    "url": "/static/media/lessons-perceptron.47155a76.jpg"
+  },
+  {
+    "revision": "cb2f65a2181811ba82d3db0ed0880d1c",
+    "url": "/static/media/materialui.cb2f65a2.svg"
+  },
+  {
+    "revision": "2aa97997a9858f2713bd8a6dd7438307",
+    "url": "/static/media/p5js.2aa97997.png"
+  },
+  {
+    "revision": "c0b399df603962eeeb5a7ab082f6b42d",
+    "url": "/static/media/page-graph.c0b399df.png"
+  },
+  {
+    "revision": "90c3286206c4f0421a3ee3a5448970ac",
+    "url": "/static/media/react.90c32862.png"
+  }
+]);
