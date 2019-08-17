05b346053541edd3407b0fc1a438385ab94e3b3b
shilangyu <xmarcinmarcin@gmail.com>
2019-08-17 23:15:26 +0200

    header refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,19 +9,22 @@ import {
 	ListItemText,
 	Menu,
 	MenuItem,
+	Slide,
 	SwipeableDrawer,
 	Toolbar,
-	Typography
+	Typography,
+	useScrollTrigger
 } from '@material-ui/core'
 import { ButtonBaseProps } from '@material-ui/core/ButtonBase'
 import { ArrowDropDown, ExpandLess, ExpandMore, Menu as MenuIcon } from '@material-ui/icons'
 import React, { Component } from 'react'
 import { Link, LinkProps } from 'react-router-dom'
 import styled from 'styled-components'
+import { Years } from '../constants/types'
 import urls from '../constants/urls'
 import { weakUId } from '../utils'
 
-const WALink: (to: string) => React.SFC<ButtonBaseProps> = to => props => (
+const WALink: (to: string) => React.FC<ButtonBaseProps> = to => props => (
 	<Link to={to} {...props as LinkProps} />
 )
 
@@ -42,56 +45,84 @@ const Title = styled(Typography)`
 	flex-grow: 1;
 `
 
-type OpenSelect = {
-	anchor: null | EventTarget
-	uid: null | string
-}
-
-type Subs = {
-	text: string
-	to: string
-}
-
 type HLink = {
 	text: string
 	uid: string
-	to?: string
-	subs?: Subs[]
+} & (
+	| {
+			to: string
+	  }
+	| {
+			subs: {
+				text: string
+				to: string
+			}[]
+	  })
+
+const links: HLink[] = [
+	{ text: 'about me', to: urls.index },
+	{ text: 'live previews', to: urls.livePreviews },
+	{
+		text: 'portfolio',
+		subs: Object.values(Years).map(text => ({
+			text,
+			to: urls.portfolio.replace(/:year$/, text)
+		}))
+	}
+].map(e => ({ ...e, uid: weakUId() }))
+
+const HideOnScroll: React.FC = ({ children }) => {
+	const trigger = useScrollTrigger()
+
+	return (
+		<Slide appear={false} direction="down" in={!trigger}>
+			{children}
+		</Slide>
+	)
 }
 
-type State = {
-	active: null | string
+type OpenSelect =
+	| {
+			anchor: EventTarget
+			uid: string
+	  }
+	| {
+			anchor: null
+			uid: null
+	  }
+
+interface State {
+	active: string
 	openSelect: OpenSelect
 	openSubList: null | string
 	drawerOpen: boolean
-	links: HLink[]
 }
 
 class Header extends Component<{}, State> {
 	state = {
-		active: null,
+		active: (() => {
+			const route = window.location.hash.substring(1)
+			const found = links.find(e => {
+				if ('subs' in e) {
+					const inSubs = e.subs.find(e => e.to.startsWith(route))
+					return !!inSubs
+				}
+				return e.to === route
+			})
+
+			return found ? found.uid : ''
+		})(),
 		openSelect: { anchor: null, uid: null },
 		openSubList: null,
-		drawerOpen: false,
-		links: [
-			{ text: 'about me', to: urls.index, uid: weakUId() },
-			{ text: 'live previews', to: urls.livePreviews, uid: weakUId() },
-			{
-				text: 'portfolio',
-				subs: [
-					{ text: '2018', to: urls.portfolio.replace(/:year$/, '2018') },
-					{ text: '2019', to: urls.portfolio.replace(/:year$/, '2019') }
-				],
-				uid: weakUId()
-			}
-		]
+		drawerOpen: false
 	}
 
-	openSubList = (id: string) =>
+	toggleSubList = (id: string) =>
 		this.setState(prevState => ({ openSubList: prevState.openSubList === id ? null : id }))
 
-	openSelect = (e: CustomEvent, id: string) =>
+	openSelect = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: string) =>
 		this.setState({ openSelect: { anchor: e.currentTarget, uid: id } })
+
 	closeSelect = () => this.setState({ openSelect: { anchor: null, uid: null } })
 
 	toggleDrawer = () => this.setState({ drawerOpen: !this.state.drawerOpen })
@@ -99,126 +130,127 @@ class Header extends Component<{}, State> {
 	setActive = (id: string) => this.setState({ active: id })
 
 	render() {
-		const { openSelect, links, active, openSubList } = this.state
+		const { openSelect, active, openSubList } = this.state
 
 		return (
 			<nav>
-				<AppBar position="sticky">
-					<Toolbar>
-						<Hidden smUp>
-							<MenuButton onClick={this.toggleDrawer} color="inherit" aria-label="Menu">
-								<MenuIcon />
-							</MenuButton>
+				<Toolbar />
+				<HideOnScroll>
+					<AppBar>
+						<Toolbar>
+							<Hidden smUp>
+								<MenuButton onClick={this.toggleDrawer} color="inherit">
+									<MenuIcon />
+								</MenuButton>
 
-							<SwipeableDrawer
-								open={this.state.drawerOpen}
-								onClose={this.toggleDrawer}
-								onOpen={this.toggleDrawer}
-							>
-								<div tabIndex={0} role="button" onKeyDown={this.toggleDrawer}>
-									<List>
-										{links.map(link =>
-											link.subs ? (
-												<React.Fragment key={link.text}>
+								<SwipeableDrawer
+									open={this.state.drawerOpen}
+									onClose={this.toggleDrawer}
+									onOpen={this.toggleDrawer}
+								>
+									<div tabIndex={0} role="button" onKeyDown={this.toggleDrawer}>
+										<List>
+											{links.map(link =>
+												'subs' in link ? (
+													<React.Fragment key={link.text}>
+														<ListItem
+															button
+															onClick={e => this.toggleSubList(link.uid)}
+															selected={link.uid === active}
+														>
+															<ListItemText primary={link.text} />
+															{openSubList === link.uid ? <ExpandLess /> : <ExpandMore />}
+														</ListItem>
+														<Collapse in={openSubList === link.uid} timeout="auto" unmountOnExit>
+															<List>
+																{link.subs.map(sub => (
+																	<Item
+																		button
+																		key={sub.text}
+																		component={WALink(sub.to)}
+																		onClick={() => {
+																			this.setActive(link.uid)
+																			this.toggleDrawer()
+																		}}
+																	>
+																		<ListItemText primary={sub.text} />
+																	</Item>
+																))}
+															</List>
+														</Collapse>
+													</React.Fragment>
+												) : (
 													<ListItem
+														onClick={() => {
+															this.setActive(link.uid)
+															this.toggleDrawer()
+														}}
+														key={link.text}
+														component={WALink(link.to)}
 														button
-														onClick={e => this.openSubList(link.uid)}
 														selected={link.uid === active}
 													>
 														<ListItemText primary={link.text} />
-														{openSubList === link.uid ? <ExpandLess /> : <ExpandMore />}
 													</ListItem>
-													<Collapse in={openSubList === link.uid} timeout="auto" unmountOnExit>
-														<List component={'div' as 'ul'} disablePadding>
-															{link.subs.map(sub => (
-																<Item
-																	button
-																	key={sub.text}
-																	component={WALink(sub.to)}
-																	onClick={() => {
-																		this.setActive(link.uid)
-																		this.toggleDrawer()
-																	}}
-																>
-																	<ListItemText primary={sub.text} />
-																</Item>
-															))}
-														</List>
-													</Collapse>
-												</React.Fragment>
-											) : (
-												<ListItem
-													onClick={() => {
-														this.toggleDrawer()
-														this.setActive(link.uid)
-													}}
-													key={link.text}
-													component={WALink(link.to)}
-													button
-													selected={link.uid === active}
-												>
-													<ListItemText primary={link.text} />
-												</ListItem>
-											)
-										)}
-									</List>
-								</div>
-							</SwipeableDrawer>
-						</Hidden>
+												)
+											)}
+										</List>
+									</div>
+								</SwipeableDrawer>
+							</Hidden>
 
-						<Title variant="h6" color="inherit">
-							Marcin Wojnarowski
-						</Title>
+							<Title variant="h6" color="inherit">
+								Marcin Wojnarowski
+							</Title>
 
-						<Hidden xsDown>
-							{links.map(link =>
-								link.subs ? (
-									<React.Fragment key={link.text}>
+							<Hidden xsDown>
+								{links.map(link =>
+									'subs' in link ? (
+										<React.Fragment key={link.text}>
+											<Button
+												aria-haspopup="true"
+												onClick={e => this.openSelect(e, link.uid)}
+												color="inherit"
+												variant={active === link.uid ? 'outlined' : undefined}
+											>
+												{link.text}
+												<DropDownIcon />
+											</Button>
+											<Menu
+												anchorEl={openSelect.anchor}
+												open={openSelect.uid === link.uid}
+												onClose={this.closeSelect}
+											>
+												{link.subs.map(sub => (
+													<MenuItem
+														key={sub.text}
+														component={WALink(sub.to)}
+														onClick={() => {
+															this.setActive(link.uid)
+															this.closeSelect()
+														}}
+													>
+														{sub.text}
+													</MenuItem>
+												))}
+											</Menu>
+										</React.Fragment>
+									) : (
 										<Button
-											aria-owns={openSelect ? link.uid : undefined}
-											aria-haspopup="true"
-											onClick={e => this.openSelect((e as unknown) as CustomEvent, link.uid)}
+											key={link.text}
+											component={WALink(link.to)}
+											onClick={() => this.setActive(link.uid)}
 											color="inherit"
 											variant={active === link.uid ? 'outlined' : undefined}
 										>
 											{link.text}
-											<DropDownIcon />
 										</Button>
-										<Menu
-											id={link.uid}
-											anchorEl={openSelect.anchor}
-											open={openSelect.uid === link.uid}
-											onClose={this.closeSelect}
-										>
-											{link.subs.map(sub => (
-												<MenuItem
-													key={sub.text}
-													component={WALink(sub.to)}
-													onClick={() => {
-														this.setActive(link.uid)
-														this.closeSelect()
-													}}
-												>
-													{sub.text}
-												</MenuItem>
-											))}
-										</Menu>
-									</React.Fragment>
-								) : (
-									<Button
-										key={link.text}
-										component={WALink(link.to) as any}
-										onClick={() => this.setActive(link.uid)}
-										color="inherit"
-										variant={active === link.uid ? 'outlined' : undefined}
-									>
-										{link.text}
-									</Button>
-								)
-							)}
-						</Hidden>
-					</Toolbar>
-				</AppBar>
+									)
+								)}
+							</Hidden>
+						</Toolbar>
+					</AppBar>
+				</HideOnScroll>
 			</nav>
 		)
 	}
