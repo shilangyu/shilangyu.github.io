063429895efb740bd10fd5c5e00a22d9ee510a06
shilangyu <xmarcinmarcin@gmail.com>
2019-12-08 14:41:11 +0000

    Deploying to master from develop e8be4b18f6cba039f66cf308e52742d8ddda44ec

---
+++ precache-manifest.9d0201cf66098c1db9fb5fb056e375cf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18382f5cbeb4508731d849e2b167708c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae8db043bd51ca59b44d",
+    "url": "/static/js/2.1e2d2e19.chunk.js"
+  },
+  {
+    "revision": "c2333b9d88d822ca861439a1a78428aa",
+    "url": "/static/js/2.1e2d2e19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "370f6cb78902de26356b",
+    "url": "/static/js/main.c7041d7d.chunk.js"
+  },
+  {
+    "revision": "60da0d09dcf9ba4795d6",
+    "url": "/static/js/runtime-main.69aca25e.js"
+  },
+  {
+    "revision": "923304ad3ae6cc73383b047d72c9ac72",
+    "url": "/static/media/DR-dataset.923304ad.png"
+  },
+  {
+    "revision": "ffdd48af507af250efffc9124476bd6e",
+    "url": "/static/media/DR-mnist.ffdd48af.png"
+  },
+  {
+    "revision": "c46482c10f0d0f177541c30072f5b955",
+    "url": "/static/media/DR-visualisation.c46482c1.png"
+  },
+  {
+    "revision": "f273e8b84021a288e38c62fb8fb10288",
+    "url": "/static/media/GA-graph.f273e8b8.png"
+  },
+  {
+    "revision": "861f778cd8c6162d5d665a8e6487ace8",
+    "url": "/static/media/NE-graph.861f778c.png"
+  },
+  {
+    "revision": "7ab02160d427bfafe395a7ce7483f59b",
+    "url": "/static/media/NNcar-laser.7ab02160.png"
+  },
+  {
+    "revision": "938e6cd3708cdf39b33f6f39d82a297a",
+    "url": "/static/media/drawio.938e6cd3.svg"
+  },
+  {
+    "revision": "a0d2ce608d9ce9bd793aab43ba02da53",
+    "url": "/static/media/geneticts.a0d2ce60.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "d9321b7c851fc0f4956af370743413e4",
+    "url": "/static/media/lessons-GA.d9321b7c.png"
+  },
+  {
+    "revision": "47155a76557a3a831177efea71d08f9f",
+    "url": "/static/media/lessons-perceptron.47155a76.jpg"
+  },
+  {
+    "revision": "cb2f65a2181811ba82d3db0ed0880d1c",
+    "url": "/static/media/materialui.cb2f65a2.svg"
+  },
+  {
+    "revision": "c0b399df603962eeeb5a7ab082f6b42d",
+    "url": "/static/media/page-graph.c0b399df.png"
+  },
+  {
+    "revision": "b0076afb7645ab84836ccb89131122a8",
+    "url": "/static/media/react.b0076afb.svg"
+  }
+]);
