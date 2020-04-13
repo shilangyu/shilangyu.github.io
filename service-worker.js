3cddb3e5999c65661eb3db8b58fd17def63c331e
shilangyu <xmarcinmarcin@gmail.com>
2020-04-13 14:12:27 +0000

    Deploying to master from develop @ 25090bf1f9ef882e8fde4294ca46701115a180f4 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3547e45cb9d58d831640a9414e820f77.js"
+  "/precache-manifest.88bbf16c631ff48d429f37f60c6725c5.js"
 );
 
 self.addEventListener('message', (event) => {
