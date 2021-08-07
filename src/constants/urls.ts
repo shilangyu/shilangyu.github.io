cd4feba0306f3f13a0a09e2d6282cae7e55579c8
shilangyu <xmarcinmarcin@gmail.com>
2021-08-07 15:49:43 +0200

    Export default reference

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,16 +1,18 @@
-export default {
-	index: '/',
-	livePreviews: '/live-previews',
-	portfolio: '/portfolio/:year',
-	external: {
-		github: 'https://github.com/shilangyu',
-		materialui: 'https://material-ui.com',
-		p5: 'http://p5js.org',
-		drawio: 'https://www.draw.io/',
-		react: 'http://reactjs.org',
-		livePreviews: {
-			tictactoe: '/TicTacToe',
-			geneticts: '/genetic.ts'
-		}
-	}
+const urls = {
+  index: '/',
+  livePreviews: '/live-previews',
+  portfolio: '/portfolio/:year',
+  external: {
+    github: 'https://github.com/shilangyu',
+    materialui: 'https://material-ui.com',
+    p5: 'http://p5js.org',
+    drawio: 'https://www.draw.io/',
+    react: 'http://reactjs.org',
+    livePreviews: {
+      tictactoe: '/TicTacToe',
+      geneticts: '/genetic.ts',
+    },
+  },
 }
+
+export default urls
